a19762b18da36b1c9c66e96a5b3c42c5327812b9
Jensi Patel <jensipatel369@gmail.com>
2025-12-18 11:24:31 +0530

    Prectice

--- Classwork/Node/index.js
+++ Classwork/Node/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const port = 2312;
 
-const app = express()
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.write("<h1>Hello <br> Welcome to Home Page...!</h1>");
+    res.end();
+})
+
+app.get("/about",(req,res)=>{
+    res.write("<h1>Welcome to About Page...!</h1>");
+    res.end();
+})
 
 app.listen(port,(err)=>{
-    err ? console.log(err) : console.log(`Server is running on port : ${port}`);
+    err ? console.log(err) : console.log(`Server is running on port ${port}`);
 })
