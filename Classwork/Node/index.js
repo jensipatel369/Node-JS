f6c612d6a8b82079f8d4220f7263001bc2f3d0bb
Jensi Patel <jensipatel369@gmail.com>
2025-12-18 11:13:13 +0530

    Prectice

--- Classwork/Node/index.js
+++ Classwork/Node/index.js
@@ -1,13 +1,8 @@
-const http = require("http");
+const express = require("express");
 const port = 2312;
 
-const postHandler = (req, res) => {
-    res.write("Hello, This is node server..!");
-    res.end();
-}
+const app = express()
 
-const server = http.createServer(postHandler);
-
-server.listen(port,(err)=>{
+app.listen(port,(err)=>{
     err ? console.log(err) : console.log(`Server is running on port : ${port}`);
 })
