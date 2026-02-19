63fd42b536e628d8f356fc09b0fefdb04a346bd6
Jensi Patel <jensipatel369@gmail.com>
2026-02-19 11:37:55 +0530

    Prectice

--- Live/JWT with react/Backend/index.js
+++ Live/JWT with react/Backend/index.js
@@ -1,32 +1,4 @@
-    // const express = require("express");
-    // const port = process.env.PORT ||2312;
-
-    // const app = express();
-    // const db = require("./config/db");
-    // const cors = require("cors");
-    // const cookie = require("cookie-parser");
-    // const session = require("express-session");
-
-    // app.use(cookie());
-    // app.use(session({
-    //     name: "local",
-    //     secret: "rnw",
-    //     resave: false,
-    //     saveUninitialized: false,
-    //     cookie: {maxAge: 1000 * 100 * 60, httpOnly: true}
-    // }))
-
-    // app.use(express.urlencoded({ extended: true }));
-    // app.use(cors({origin: "http://localhost:5173", credentials: true}));
-    // app.use(express.json());
-
-    // app.use("/", require("./routes/route"))
-
-    // app.listen(port, (err) => {
-    //     err ? console.log(err) : console.log(`Server is running on port ${port}`);
-    // })
-
-    const express = require("express");
+const express = require("express");
 const port = process.env.PORT || 2312;
 
 const app = express();
@@ -34,35 +6,22 @@ const db = require("./config/db");
 const cors = require("cors");
 const cookie = require("cookie-parser");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
-
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
 
 app.use(cookie());
-
 app.use(session({
     name: "local",
     secret: "rnw",
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl: "mongodb://127.0.0.1:27017/your_db_name"
-    }),
-    cookie: {
-        maxAge: 1000 * 60 * 60,
-        httpOnly: true
-    }
-}));
+    cookie: { maxAge: 1000 * 100 * 60, httpOnly: true }
+}))
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
-app.use("/", require("./routes/route"));
+app.use("/", require("./routes/route"))
 
 app.listen(port, (err) => {
-    err ? console.log(err) :
-    console.log(`Server running on port ${port}`);
-});
+    err ? console.log(err) : console.log(`Server is running on port ${port}`);
+})
