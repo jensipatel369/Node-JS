264c0583cf89176292bcb96b62df934cce7e9365
Jensi Patel <jensipatel369@gmail.com>
2026-02-26 09:24:30 +0530

    Prectice

---
+++ Project/Final Project/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const port = 2312;
+const path = require("path")
+
+const app = express();
+const db = require("./config/db");
+const cookie = require("cookie-parser")
+const passport = require("passport")
+const session = require("express-session")
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+app.use(express.static(path.join(__dirname,"public")))
+app.use(cookie())
+
+app.use(
+  session({
+    name : "local",
+    secret: "rnw",
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge : 100*100*60, httpOnly:true },
+  })
+);  
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.use("/", require("./routes/route"));
+app.use("/category", require("./routes/categoryRoutes"));
+app.use("/subCategory", require("./routes/subCatRoute"));
+app.use("/products", require("./routes/productRoute"));
+
+app.listen(port, (err) => {
+  err ? console.log(err) : console.log(`Server is started on port : ${port}`);
+});
+  
